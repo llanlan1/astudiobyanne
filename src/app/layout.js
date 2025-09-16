06e965b88cf259bb30f20ab03d6727db020c3615
llanlan1 <llanlan91@gmail.com>
2025-09-16 18:00:57 +0800

    Refactor font integration by moving Playfair Display font declaration outside of the RootLayout function for improved performance and readability.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,18 @@
 import "./globals.css";
 import { Playfair_Display } from "next/font/google";
 
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  display: "swap",
+});
+
 export const metadata = {
   title: "A STUDIO BY ANNE",
   description: "A private art practice showcasing beauty and art at the forefront. Based in Singapore.",
 };
 
 export default function RootLayout({ children }) {
-  const playfair = Playfair_Display({
-    subsets: ["latin"],
-    weight: ["300", "400", "700"],
-    display: "swap",
-  });
   return (
     <html lang="en">
       <head></head>
