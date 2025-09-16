2d5cb1c35c68648f9440536f3e61346ed00dc093
llanlan1 <llanlan91@gmail.com>
2025-09-16 18:05:39 +0800

    Update Playfair Display font weights to remove unused weight for improved performance.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Playfair_Display } from "next/font/google";
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
-  weight: ["300", "400", "700"],
+  weight: ["400", "700"],
   display: "swap",
 });
 
